6fed31d874f1d21c8195bf81ab2c585c46e0a603
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-07-02 10:24:53 -0300

    a

--- ecommerce/mercado/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
-db.init_app(app)
-
-from mercado import routes
--- ecommerce/mercado/forms.py
+++
@@ -1,5 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-
-class CadastroForm(FlaskForm):
-    usuario = StringField(label='Username:')
